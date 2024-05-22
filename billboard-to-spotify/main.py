7b36ba9b52d84ab7fa5d3733850f40c6a114a5fd
Rabi Bhat <itsrabio7@gmail.com>
2024-05-22 15:41:12 -0400

    scrapping playlist: song list created based on user input date

--- billboard-to-spotify/main.py
+++ billboard-to-spotify/main.py
@@ -12,3 +12,11 @@ sp = Spotify(auth_manager=SpotifyOAuth(client_id="0b34a21a6fa746079e02c3daf617de
 
 user_id = sp.current_user()['id']
 
+# Scrapping songs from bill board
+user_input = input("Which year you want to travel to?(YYYY-MM-DD): ")
+res = requests.get(url=f"https://www.billboard.com/charts/hot-100/{user_input}")
+songs_data = BeautifulSoup(res.text, 'html.parser')
+playlist = songs_data.select('div ul li ul li h3')
+playlist = [song.getText().strip() for song in playlist]
+
+
