c67de6a396f6a5d9444f71fb71c522d59f3b74a3
Rabi Bhat <itsrabio7@gmail.com>
2024-05-25 18:47:02 -0400

    item purchase: logic added for purchasing the items to maximize the production

--- cookie-clicker/main.py
+++ cookie-clicker/main.py
@@ -16,15 +16,32 @@ cookie = driver.find_element(By.ID, value='cookie')
 # Some functions
 def purchase_items():
     money = driver.find_element(By.ID, value='money')
-    available_items_obj = driver.find_elements(By.CSS_SELECTOR, value='#store div b')
-    available_items = [item.text for item in available_items_obj]
-    available_items.pop()
-    available_items = [int(item.split(' - ')[1].replace(',', '')) for item in available_items]
+    available_items = driver.find_elements(By.CSS_SELECTOR, value='#store div')
+    item_cost_list_obj = driver.find_elements(By.CSS_SELECTOR, value='#store div b')
+    item_cost_list = [item.text for item in item_cost_list_obj]
+    item_cost_list.pop()
+    item_cost_list = [int(item.split(' - ')[1].replace(',', '')) for item in item_cost_list]
+
+    # converting money to int
+    try:
+        money = int(money.text)
+    except ValueError:
+        money = money.text.replace(',', '')
+        money = int(money)
+
+    # Filtering available_items for only valid items
+    available_items = [item for item in available_items if item.get_attribute('onclick') is not None]
 
     # checking which item can be bought
     # checking from most expensive item since we want to buy expensive one first if possible
-    for item in reversed(available_items):
-        print(item)
+    for item_cost in reversed(item_cost_list):
+        if item_cost <= int(money):
+            print(item_cost, money)
+            idx = item_cost_list.index(item_cost)
+            buy_item = available_items[idx]
+            print(buy_item.get_attribute('id'))
+            buy_item.click()
+            break
 
 
 # main loop and setting 5sec delay for purchasing the items
