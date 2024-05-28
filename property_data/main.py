2449902e3c2bc8d05183fce954eee23a3f996199
Rabi Bhat <itsrabio7@gmail.com>
2024-05-28 19:37:47 -0400

    form fill: google forms were filled with all the data extracted

--- property_data/main.py
+++ property_data/main.py
@@ -21,3 +21,32 @@ property_price_list = [price.text.replace('+/mo', '').replace('+ 1 bd', '').repl
 property_url_list = [url.get('href') for url in property_url_list]
 # print(property_addr_list[0].replace(', 747 Geary St', ''))
 
+
+# creating a chrome driver with selenium
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(options=chrome_options)
+
+# filling google form
+for i in range(len(property_addr_list)):
+    driver.get('https://forms.gle/TSYipRGDqEBC46bMA')
+
+    # getting form fields from google forms
+    addr_field = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div['
+                                                     '2]/div/div[1]/div/div[1]/input')
+    price_field = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div['
+                                                      '2]/div/div[1]/div/div[1]/input')
+    url_field = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div['
+                                                    '2]/div/div[1]/div[2]/textarea')
+
+    submit_btn = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+
+    # populating the form fields
+    addr_field.send_keys(property_addr_list[i])
+    price_field.send_keys(property_price_list[i])
+    url_field.send_keys(property_url_list[i])
+    submit_btn.click()
+    print(f'Sending data set {i}')
+    time.sleep(1)
+
+print('Program completed!')
