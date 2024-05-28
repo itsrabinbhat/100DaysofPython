39d8e4dae3b8e4aba878095b11bdfb2d01732d66
Rabi Bhat <itsrabio7@gmail.com>
2024-05-28 19:36:48 -0400

    data cleanup: extractd data was cleaned up ready to fill form

--- property_data/main.py
+++ property_data/main.py
@@ -14,4 +14,10 @@ property_addr_list = sp.find_all('address', attrs={'data-test': 'property-card-a
 property_price_list = sp.find_all('span', attrs={'data-test': 'property-card-price'})
 property_url_list = sp.find_all('a', attrs={'data-test': 'property-card-link'})
 
+# Cleaning up the extracted data
+property_addr_list = [addr.text.strip() for addr in property_addr_list]
+property_price_list = [price.text.replace('+/mo', '').replace('+ 1 bd', '').replace('/mo', '').replace('+ 1bd', '') for
+                       price in property_price_list]
+property_url_list = [url.get('href') for url in property_url_list]
+# print(property_addr_list[0].replace(', 747 Geary St', ''))
 
