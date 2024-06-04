27abb2ed4d89592ca5f087b287de9b2e9423d2dc
Rabi Bhat <itsrabio7@gmail.com>
2024-06-04 16:53:57 -0400

    clear screen: handeling clear screen for any os

--- text_to_morse/main.py
+++ text_to_morse/main.py
@@ -33,28 +33,36 @@ def decode_morse(morse_code):
     return decoded_text
 
 
+# Clearing the screen
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
 # Creating main function to handle user input and result output
 def main():
-    os.system('cls')
+    clear_screen()
     print('--------------WELCOME TO OUT MORSE CODE TRANSLATOR------------------')
     print('Available options:\n'
           '1. Encode text\n2. Decode text\n3. Exit')
     user_choice = input("Enter your choice(1, 2 or 3): ")
-    if int(user_choice) == 1:
-        os.system('cls')
+    if int(user_choice.strip()) == 1:
+        clear_screen()
         to_encode = input("Enter a string to convert it to morse code or enter 'X' to go to main menu: ")
         encoded_text = encode_text(to_encode)
         print(f'\nString: {to_encode}\nMorse code: {encoded_text}')
-        if input('\nEnter any key to continue: '):
+        if input('\nEnter any key to continue or Enter to exit: '):
             main()
-    elif int(user_choice) == 2:
-        os.system('cls')
+    elif int(user_choice.strip()) == 2:
+        clear_screen()
         to_decode = input("Enter morse code to convert it to string or enter 'X' to go to main menu: ")
         decoded_text = decode_morse(to_decode)
         print(f'\nMorse code: {to_decode} \nDecoded String: {decoded_text}')
-        if input('\nEnter any key to continue: '):
+        if input('\nEnter any key to continue or Enter to exit: '):
             main()
-    elif int(user_choice) == 3:
+    elif int(user_choice.strip()) == 3:
         exit()
     else:
         print('Invalid choice! Try again...')
