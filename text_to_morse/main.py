ede90e21c44b57dfc912ee56cdcc8905aee04f43
Rabi Bhat <itsrabio7@gmail.com>
2024-06-04 16:44:58 -0400

    main: created main function to handle input, output and program flow

--- text_to_morse/main.py
+++ text_to_morse/main.py
@@ -33,3 +33,32 @@ def decode_morse(morse_code):
     return decoded_text
 
 
+# Creating main function to handle user input and result output
+def main():
+    os.system('cls')
+    print('--------------WELCOME TO OUT MORSE CODE TRANSLATOR------------------')
+    print('Available options:\n'
+          '1. Encode text\n2. Decode text\n3. Exit')
+    user_choice = input("Enter your choice(1, 2 or 3): ")
+    if int(user_choice) == 1:
+        os.system('cls')
+        to_encode = input("Enter a string to convert it to morse code or enter 'X' to go to main menu: ")
+        encoded_text = encode_text(to_encode)
+        print(f'\nString: {to_encode}\nMorse code: {encoded_text}')
+        if input('\nEnter any key to continue: '):
+            main()
+    elif int(user_choice) == 2:
+        os.system('cls')
+        to_decode = input("Enter morse code to convert it to string or enter 'X' to go to main menu: ")
+        decoded_text = decode_morse(to_decode)
+        print(f'\nMorse code: {to_decode} \nDecoded String: {decoded_text}')
+        if input('\nEnter any key to continue: '):
+            main()
+    elif int(user_choice) == 3:
+        exit()
+    else:
+        print('Invalid choice! Try again...')
+        main()
+
+
+main()
